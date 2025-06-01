8540403e6a8eff4bcfb61466595d2a7fce312d4d
Danylo <shomdan2004@gmail.com>
2025-06-01 17:20:46 +0300

    Cleaned up client-only branch for user version

--- server/levels/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Level
-
-class LevelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Level
-        fields = ['id', 'author', 'title', 'description', 'data', 'created_at', 'is_public']
-        read_only_fields = ['id', 'author', 'created_at']
