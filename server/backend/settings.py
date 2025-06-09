97d24dc0d6f013bba4f58cd42e4577b69dc845ce
Danylo <shomdan2004@gmail.com>
2025-06-09 23:56:35 +0300

    Enable CORS for frontend on render.com

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
     'users',
     'levels',
+    'corsheaders',
 ]
 
 
@@ -33,6 +34,8 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -43,6 +46,15 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://levelking-client.onrender.com"
+]
+
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+]
 
 TEMPLATES = [
     {
