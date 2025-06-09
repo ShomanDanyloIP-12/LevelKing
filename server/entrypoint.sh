a2333b6819b476306d3613cb0b52e81e4e8b05bc
Danylo <shomdan2004@gmail.com>
2025-06-10 00:14:19 +0300

    Auto-create superuser in entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 python manage.py migrate --noinput
+
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin', 'shomdan2004@gmail.com', 'admin123')" \
+| python manage.py shell
+
 python manage.py runserver 0.0.0.0:8000
